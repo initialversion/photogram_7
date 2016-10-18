97bd415f2ae39bb50e656397fea52d3bf1ce1316
Murugan <murugu2012@gmail.com>
2016-10-18 20:42:35 +0530

    Validate uniqueness of votes.photo_id

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,6 +5,8 @@ class Vote < ApplicationRecord
 
   # Validations
 
+  validates :photo_id, :uniqueness => { :scope => [:voter_id], :message => "already liked" }
+
   validates :photo_id, :presence => true
 
   validates :voter_id, :presence => true
