0b247e2d3ef21f5f1c41a6dc7e5e0ec55c13539d
Murugan <murugu2012@gmail.com>
2016-10-18 20:43:59 +0530

    Whitelist columns in Orange for ActiveAdmin

--- app/admin/orange.rb
+++ app/admin/orange.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Orange do
 
+ permit_params :vote_id, :orange_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
